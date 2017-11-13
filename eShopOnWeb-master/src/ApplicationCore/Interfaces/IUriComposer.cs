e46323000854f3f6e0b255cac9426e4e7fdd400f
Ed Hawkes <holaamigoquepasa@gmail.com>
2017-11-12 22:48:45 -0700

    Initial revision

---
+++ eShopOnWeb-master/src/ApplicationCore/Interfaces/IUriComposer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Entities;
+using System.Collections.Generic;
+
+namespace ApplicationCore.Interfaces
+{
+
+    public interface IUriComposer
+    {
+        string ComposePicUri(string uriTemplate);
+    }
+}
---
+++ eShopOnWeb-master/src/Web/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://0.0.0.0:5106")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole(LogLevel.Warning);
+                    factory.AddDebug();
+                })
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
